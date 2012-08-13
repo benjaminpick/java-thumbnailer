ddfc73d31dd77603b98c260b47c3ff20a2e38e09
Benjamin Pick <benjamin4ruby@github.com>
2012-08-13 12:10:02 +0200

    Licence change: GPL 2.1 -> 3

--- src/de/uni_siegen/wineme/come_in/thumbnailer/util/mime/MimeTypeIdentifier.java
+++ src/de/uni_siegen/wineme/come_in/thumbnailer/util/mime/MimeTypeIdentifier.java
@@ -1,11 +1,11 @@
 /*
  * regain/Thumbnailer - A file search engine providing plenty of formats (Plugin)
- * Copyright (C) 2011  Come_IN Computerclubs (University of Siegen)
+ * Copyright (C) 2011-2012  Come_IN Computerclubs (University of Siegen)
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * version 3 of the License, or (at your option) any later version.
  *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
